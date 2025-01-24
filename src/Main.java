dc4d13af4c405d507ff19ad10b6409d9f12def0f
Itayr <ir6922@ld.amalnet.k12.il>
2025-01-24 12:47:57 +0200

    added test Bdika

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Date date1 = new Date();
-        Date date2 = new Date();
-        System.out.println(date1.compareTo(date2));
-
-    }
-}
